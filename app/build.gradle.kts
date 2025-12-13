83380fe5f51fe37d036aa5e8a6cd4267a9375c69
Harish Penta <harish@eightfolds.io>
2025-12-13 07:11:09 +0530

    App Icons changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,13 +10,13 @@ plugins {
 
 android {
     namespace = "com.pentadigital.calculator"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.pentadigital.calculator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
